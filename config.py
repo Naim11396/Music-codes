86f907d03fad6a46430069b70faa01deee828316
Mustihere <mustiisback95@gmail.com>
2024-10-12 17:47:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ que = {}
 admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "") 
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+API_ID = int(getenv("API_ID", "27305765"))
+API_HASH = getenv("API_HASH", "c3d9e1be50a6777fae7fadd3f156e15a")
+BOT_TOKEN = getenv("BOT_TOKEN", "8094349979:AAGxJ29LU8fuUN6pJFHqiIiiMK8ZU8JburU")
+BOT_NAME = getenv("BOT_NAME", "Music Bot") 
+BOT_USERNAME = getenv("BOT_USERNAME", "@mustimuzik_bot")
 BOT_CHANNEL = getenv("BOT_CHANNEL", "")
 ASS_USERNAME = getenv("ASS_USERNAME", "")
-OWNER = getenv("OWNER", "")
+OWNER = getenv("OWNER", "7194000526")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", 500))
 
